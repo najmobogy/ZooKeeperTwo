378a2330d92a49f5d4005b91310be114707ac2d2
Bog <najmobogi@gmail.com>
2025-03-02 18:43:00 +0100

    afera

--- src/controllers/AnimalsController.ts
+++ src/controllers/AnimalsController.ts
@@ -5,57 +5,57 @@ import {Animal} from "../models/animal";
 class AnimalController{
     static GetAllAnimals(req: Request, res: Response): void{
         const animals = animalService.GetAllAnimals()
-        res.json({message: "all animals", animals})
+        res.json({message: "wszystkie zwierzeta", animals})
     }
     static GetAllAnimalsEndagered(req: Request, res: Response): void{
         const endageredAnimals = animalService.GetAllAnimalsEndagered()
-        res.json({message: "all endagered animals", endageredAnimals})
+        res.json({message: "wszystkie zwierzeta zagrozone", endageredAnimals})
 
     }
     static GetAllAnimalsInHabitat(req: Request, res: Response): void{
         const habitat: string = req.params.habitat
         const animalsHabitat = animalService.GetAllAnimalsInHabitat(habitat)
         if(animalsHabitat.length != 0){
-            res.json({message: "all animals in habitat", animalsHabitat})
+            res.json({message: "Wszystkie zwierzaki", animalsHabitat})
         }
         else {
-            res.status(400).json({message: "There is an error in your request. Check and try again"})
+            res.status(400).json({message: "ERROR"})
         }
     }
     static GetAllAnimalsSpecies(req: Request, res: Response): void{
         const species:string = req.query.species as string
         const animalsSpecies = animalService.GetAllAnimalsSpecies(species)
         if(animalsSpecies.length != 0){
-            res.json({message: "all animals in habitat", animalsSpecies})
+            res.json({message: "zwierzaki", animalsSpecies})
         }
         else {
-            res.status(400).json({message: "There is an error in your request. Check and try again"})
+            res.status(400).json({message: "ERROR"})
         }
     }
     static PostNewAnimal(req: Request, res: Response): void{
         const newAnimal: Animal = req.body
         const PostAnimal = animalService.PostNewAnimal(newAnimal)
-        res.json({message: "We added your animal", PostAnimal})
+        res.json({message: "Dodanie zwierzaczka", PostAnimal})
     }
     static UpdateAnimal(req: Request, res: Response): void{
         const id: number = parseInt(req.params.id)
         const UpdatedAnimal: Animal = req.body
         const result = animalService.UpdateAnimal(id, UpdatedAnimal)
         if(result != null){
-            res.json({message: "We updated animal", result})
+            res.json({message: "aktualizacja zwierzaczka", result})
         }
         else{
-            res.status(400).json({message: "There is an error in your request. Check and try again"})
+            res.status(400).json({message: "ERROR"})
         }
     }
     static DeleteAnimal(req: Request, res: Response): void{
         const id: number = parseInt(req.params.id)
         const result = animalService.DeleteAnimal(id)
         if(result){
-            res.json({message: `We deleted animal with id numer: ${id} :c \uf5b4`})
+            res.json({message: `Usunięcie zwierzecia o id: ${id} :c \uf5b4`})
         }
         else{
-            res.status(400).json({message: "There is an error in your request. Check and try again"})
+            res.status(400).json({message: "ERROR"})
         }
     }
 }
